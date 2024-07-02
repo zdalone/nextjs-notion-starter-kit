471299a4c438ae8535232523b24134bb33891909
zdalone <1035430185@qq.com>
2024-07-02 11:26:35 +0800

    Update site.config.ts
    update basic site info

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '旖墨含章',
+  domain: 'ymhz.it',
+  author: 'Big Big Dan',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
